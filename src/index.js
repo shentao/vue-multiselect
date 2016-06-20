f93f6506393b12d010cef99dc236566f93ff012c
Asvae <ontrew@gmail.com>
2016-06-20 18:54:14 +0300

    Import improvements

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
  * (c) 2016 Damian Dulisz
  * Released under the MIT License.
  */
-import Multiselect from './Multiselect'
-import multiselectMixin from './multiselectMixin'
-import pointerMixin from './pointerMixin'
+import _Multiselect from './Multiselect'
+import _multiselectMixin from './multiselectMixin'
+import _pointerMixin from './pointerMixin'
 
-module.exports = {
-  Multiselect: Multiselect,
-  multiselectMixin: multiselectMixin,
-  pointerMixin: pointerMixin
-}
+export default _Multiselect
+
+export var Multiselect = _Multiselect
+export var multiselectMixin = _multiselectMixin
+export var pointerMixin = _pointerMixin
