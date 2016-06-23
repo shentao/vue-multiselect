b4c1b2a00025814f6dab97124f3ca8837b0570ce
Damian Dulisz <damian.dulisz@monterail.com>
2016-06-23 23:12:46 +0200

    Split docs into jade partials, improve rendering

--- main.js
+++ main.js
@@ -1,8 +1,101 @@
 import Vue from 'vue'
-import App from './App'
+
+import Multiselect from '../src/Multiselect'
+import countries from './data/countries.json'
+
+require('./docs.scss')
+
+const SL = ', 100%, 87%'
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      options: ['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched'],
+      selected: ['Select option'],
+      source: [
+        { name: 'Vue.js', language: 'JavaScript' },
+        { name: 'Rails', language: 'Ruby' },
+        { name: 'Sinatra', language: 'Ruby' },
+        { name: 'Laravel', language: 'PHP' },
+        { name: 'Phoenix', language: 'Elixir' }
+      ],
+      value: { name: 'Vue.js', language: 'Javascript' },
+      valuePrimitive: 'showLabels',
+      multiValue: [{ name: 'Vue.js', language: 'Javascript' }],
+      multiple: true,
+      taggingOptions: [{ name: 'Vue.js', code: 'vu' }, { name: 'Javascript', code: 'js' }, { name: 'Monterail', code: 'pl' }, { name: 'Open Source', code: 'os' }],
+      taggingSelected: [],
+      searchable: true,
+      placeholder: 'Select props',
+      countries: [],
+      selectedCountries: [],
+      first: 230,
+      second: 197,
+      actions: ['alert', 'console.log', 'scrollTop'],
+      action: null,
+      isTouched: false,
+      exampleValue6: []
+    }
+  },
+  computed: {
+    gradient () {
+      // return 'linear-gradient(to left bottom, #C1C6FF 0%, #E7FFEB 100%) fixed'
+      return `linear-gradient(to left bottom, hsl(${this.first + SL}) 0%, hsl(${this.second + SL}) 100%)`
+    },
+    isInvalid () {
+      return this.isTouched && this.exampleValue6.length === 0
+    }
+  },
+  methods: {
+    asyncFind (query) {
+      if (query.length === 0) {
+        this.countries = []
+      } else {
+        setTimeout(() => {
+          this.countries = countries.filter((element, index, array) => {
+            return element.name.toLowerCase().includes(query.toLowerCase())
+          })
+        }, 1000)
+      }
+    },
+    afterChange (selectValue) {
+      this.selected = selectValue
+    },
+    onTagging (newTag) {
+      this.options.push(newTag)
+      this.selected.push(newTag)
+    },
+    addTag (newTag) {
+      const tag = {
+        name: newTag,
+        code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
+      }
+      this.taggingOptions.push(tag)
+      this.taggingSelected.push(tag)
+    },
+    updateSelectedTagging (value) {
+      this.taggingSelected = value
+    },
+    dispatchAction (actionName) {
+      switch (actionName) {
+        case 'alert':
+          window.alert('You just dispatched "alert" action!')
+          break
+        case 'console.log':
+          console.log('You just dispatched "console.log" action!')
+          break
+        case 'scrollTop':
+          window.scrollTo(0, 0)
+          break
+      }
+    },
+    nameWithLang ({ name, language }) {
+      return `${name} — [${language}]`
+    }
+  }
 })
