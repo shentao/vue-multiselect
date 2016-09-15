aa806e97799409e527844189aa2ced3c3f1cb44c
Nicolas Turlais <nicolas.turlais@gmail.com>
2016-09-15 12:53:44 +0200

    :D

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function (config) {
     frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
     files: [
-      '../../node_modulOes/phantomjs-polyfill-includes/includes-polyfill.js',
+      '../../node_modules/phantomjs-polyfill-includes/includes-polyfill.js',
       './index.js'
     ],
     preprocessors: {
