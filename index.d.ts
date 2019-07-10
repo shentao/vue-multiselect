e86349524c071bb8cf67f275baff2cf35edf7b59
Niel Hekkens <niel@studyportals.com>
2019-07-10 16:18:20 +0200

    Add type definitions to support TS

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+declare class Multiselect extends Vue { }
+declare class multiselectMixin extends Vue { }
+declare class pointerMixin extends Vue { }
+
+export default Multiselect;
+export { Multiselect, multiselectMixin, pointerMixin };
