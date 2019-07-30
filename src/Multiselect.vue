58e7634afbc42dbaf9f82da56185d73158fac745
Patrick Norris <patn97@gmail.com>
2019-07-30 17:56:00 -0400

    Accessibility changes: fix for screen readers not identifying options properly (#1007)
    * Accessibility (#1)
    
    Use aria-role and aria-activedescendant in order to shift DOM focus properly when using JAWS screen reader.
    
    * remove some deprecated code

--- src/Multiselect.vue
+++ src/Multiselect.vue
@@ -8,7 +8,9 @@
     @keydown.self.up.prevent="pointerBackward()"
     @keypress.enter.tab.stop.self="addPointerElement($event)"
     @keyup.esc="deactivate()"
-    class="multiselect">
+    class="multiselect"
+    role="combobox"
+    :aria-owns="'listbox-'+id">
       <slot name="caret" :toggle="toggle">
         <div @mousedown.prevent.stop="toggle()" class="multiselect__select"></div>
       </slot>
@@ -64,6 +66,7 @@
           @keypress.enter.prevent.stop.self="addPointerElement($event)"
           @keydown.delete.stop="removeLastElement()"
           class="multiselect__input"
+          :aria-controls="'listbox-'+id"
         />
         <span
           v-if="isSingleLabelVisible"
@@ -94,7 +97,7 @@
           :style="{ maxHeight: optimizedHeight + 'px' }"
           ref="list"
         >
-          <ul class="multiselect__content" :style="contentStyle">
+          <ul class="multiselect__content" :style="contentStyle" role="listbox" :id="'listbox-'+id">
             <slot name="beforeList"></slot>
             <li v-if="multiple && max === internalValue.length">
               <span class="multiselect__option">
@@ -102,7 +105,11 @@
               </span>
             </li>
             <template v-if="!max || internalValue.length < max">
-              <li class="multiselect__element" v-for="(option, index) of filteredOptions" :key="index">
+              <li class="multiselect__element"
+                v-for="(option, index) of filteredOptions"
+                :key="index"
+                v-bind:id="id + '-' + index"
+                v-bind:role="!(option && (option.$isLabel || option.$isDisabled)) ? 'option' : null">
                 <span
                   v-if="!(option && (option.$isLabel || option.$isDisabled))"
                   :class="optionHighlight(index, option)"
