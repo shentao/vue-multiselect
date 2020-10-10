fa39f5e85b043d020197f31bbcf4907f9d9f1a7e
yash ojha <yashojha@yashs-MacBook-Pro.local>
2020-10-11 01:54:17 +0530

    Added fix for options dropdown position for second time

--- src/directives/appendToBody.js
+++ src/directives/appendToBody.js
@@ -1,26 +1,55 @@
 export default {
   inserted (el, bindings, { context, child }) {
+    let parentCount = 5 // remove scroll up to 5 parents by default
+    let optionsZIndex = 999 // set zindex of options to 999 by default
+    let overflowStates = []
+    if (!bindings.value) {
+      return false
+    } else if (typeof bindings.value === 'object') {
+      if (bindings.value.value === false) {
+        return false
+      }
+      parentCount = bindings.value.parentCount || parentCount
+      optionsZIndex = bindings.value.optionsZIndex || optionsZIndex
+    }
+
     child.$watch('isOpen', isOpen => {
       if (isOpen) {
         context.$children.map(chld => {
           const optionsRef = chld.$refs.list
           if (optionsRef) {
-            const { top, left, width } = optionsRef.getBoundingClientRect()
+            const { top, left, width, height } = el.getBoundingClientRect()
             let scrollX = window.scrollX || window.pageXOffset
             let scrollY = window.scrollY || window.pageYOffset
-
             optionsRef.setAttribute(
               'style',
               `position:absolute;
                width:${width}px;
                left:${scrollX + left}px;
-               top:${scrollY + top}px;
-               z-index:999`
+               top:${scrollY + top + height}px;
+               z-index:${optionsZIndex}`
             )
+            let tempIndex = Number(parentCount)
+
+            let parentNode = el.parentNode
+            while (tempIndex) {
+              overflowStates[tempIndex] = parentNode.style.overflow
+              parentNode.style.overflow = 'hidden'
+              parentNode = parentNode.parentNode
+              tempIndex -= 1
+            }
             document.body.appendChild(optionsRef)
             el._optionsRef = optionsRef
           }
         })
+      } else {
+        let parentNode = el.parentNode
+        let tempIndex = Number(parentCount)
+        while (tempIndex) {
+          parentNode.style.overflow = overflowStates[tempIndex]
+          parentNode = parentNode.parentNode
+          tempIndex -= 1
+        }
       }
     })
   },
