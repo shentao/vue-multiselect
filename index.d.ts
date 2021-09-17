5748632c58bda7b147d1b2506df16015a84123de
Matt Elen <matt@suade.org>
2021-09-17 17:00:43 +0100

    Add TS Types

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions originally for Vue-Multislect 2.1.0, rebuilt and tested with Vue 3 version
+// Originally written by: Akshay Jat https://github.com/akki-jat
+import Vue from 'vue';
+
+declare class Multiselect extends Vue { }
+declare class multiselectMixin extends Vue { }
+declare class pointerMixin extends Vue { }
+
+export default Multiselect;
+export { Multiselect, multiselectMixin, pointerMixin };
