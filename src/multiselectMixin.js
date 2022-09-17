a7946236340a70bf9b45543b96bba5afdc19c80e
Jose Vicente <jvortsromero@gmail.com>
2022-09-17 12:45:20 +0200

    Added new option to prevent autofocus on input search

--- src/multiselectMixin.js
+++ src/multiselectMixin.js
@@ -315,6 +315,15 @@ export default {
     preselectFirst: {
       type: Boolean,
       default: false
+    },
+    /**
+     * Prevent autofocus
+     * @default false
+     * @type {Boolean}
+    */
+    preventAutofocus: {
+      type: Boolean,
+      default: false
     }
   },
   mounted () {
@@ -657,8 +666,8 @@ export default {
       /* istanbul ignore else  */
       if (this.searchable) {
         if (!this.preserveSearch) this.search = ''
-        this.$nextTick(() => { if (typeof this.$refs.search !== 'undefined') this.$refs.search.focus() })
-      } else {
+        if (!this.preventAutofocus) this.$nextTick(() => this.$refs.search && this.$refs.search.focus())
+      } else if (!this.preventAutofocus) {
         if (typeof this.$el !== 'undefined') this.$el.focus()
       }
       this.$emit('open', this.id)
