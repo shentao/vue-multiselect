ea6a3018641c6957ac890b9f77ddff50d54f72a7
Matt Elen <matt@suade.org>
2023-03-24 17:30:22 +0000

    refactor(docs): Re-add Docs

---
+++ documentation/partials/examples/index.js
@@ -0,0 +1,27 @@
+import SingleSelectPrimitive from './SingleSelectPrimitive.vue'
+import SingleSelectObject from './SingleSelectObject.vue'
+import SingleSelectSearch from './SingleSelectSearch.vue'
+import MultiSelect from './MultiSelect.vue'
+import AjaxSearch from './AjaxSearch.vue'
+import Tagging from './Tagging.vue'
+import CustomOption from './CustomOption.vue'
+import VuexActions from './VuexActions.vue'
+import CustomConfig from './CustomConfig.vue'
+import Groups from './Groups.vue'
+import ActionDispatcher from './ActionDispatcher.vue'
+import ProgrammaticCtrl from './ProgrammaticCtrl.vue'
+
+export {
+  SingleSelectPrimitive,
+  SingleSelectObject,
+  SingleSelectSearch,
+  MultiSelect,
+  AjaxSearch,
+  Tagging,
+  CustomOption,
+  VuexActions,
+  CustomConfig,
+  Groups,
+  ActionDispatcher,
+  ProgrammaticCtrl
+}
