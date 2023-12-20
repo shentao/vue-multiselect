3402b24dc9b63e106ea6416035f5bc5440367aab
Matt Elen <matt@suade.org>
2023-12-20 17:51:20 +0000

    docs(example): Fix auto-scroll menu selector

--- documentation/src/components/NavMenu.vue
+++ documentation/src/components/NavMenu.vue
@@ -1,29 +1,31 @@
 <template>
   <ul class="list" :class="{ 'list--sticky': isNavSticky }">
-    <multiselect class="list__multiselect" :options="versions" :model-value="version" :allow-empty="false" :searchable="false"
-                 @update:model-value="pickVersion" select-label="Go to" deselect-label="" selected-label=""></multiselect>
+    <multiselect class="list__multiselect" :options="versions" :model-value="version" :allow-empty="false"
+                 :searchable="false"
+                 @update:model-value="pickVersion" select-label="Go to" deselect-label=""
+                 selected-label=""></multiselect>
     <li class="list__heading">Setup</li>
-    <li class="list__element"><a href="#sub-getting-started" class="link list__link list__link--active">Getting
+    <li class="list__element"><a href="#sub-getting-started" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-getting-started' }">Getting
       Started</a></li>
-    <li class="list__heading"><a href="#examples" class="link blank__link">Examples</a></li>
-    <li class="list__element"><a href="#sub-single-select" class="link list__link">Single select</a></li>
-    <li class="list__element"><a href="#sub-single-select-object" class="link list__link">Single select (object)</a>
+    <li class="list__heading"><a href="#examples" class="link blank__link" :class="{ 'list__link--active': currentPosition === 'sub-examples' }">Examples</a></li>
+    <li class="list__element"><a href="#sub-single-select" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-single-select' }">Single select</a></li>
+    <li class="list__element"><a href="#sub-single-select-object" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-single-select-object' }">Single select (object)</a>
     </li>
-    <li class="list__element"><a href="#sub-select-with-search" class="link list__link">Select with search</a></li>
-    <li class="list__element"><a href="#sub-multiple-select" class="link list__link">Multiple select</a></li>
-    <li class="list__element"><a href="#sub-asynchronous-select" class="link list__link">Asynchronous select</a></li>
-    <li class="list__element"><a href="#sub-tagging" class="link list__link">Tagging</a></li>
-    <li class="list__element"><a href="#sub-custom-option-template" class="link list__link">Custom option template</a>
+    <li class="list__element"><a href="#sub-select-with-search" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-select-with-search' }">Select with search</a></li>
+    <li class="list__element"><a href="#sub-multiple-select" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-multiple-select' }">Multiple select</a></li>
+    <li class="list__element"><a href="#sub-asynchronous-select" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-asynchronous-select' }">Asynchronous select</a></li>
+    <li class="list__element"><a href="#sub-tagging" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-tagging' }">Tagging</a></li>
+    <li class="list__element"><a href="#sub-custom-option-template" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-custom-option-template' }">Custom option template</a>
     </li>
-    <li class="list__element"><a href="#sub-option-groups" class="link list__link">Option groups</a></li>
-    <li class="list__element"><a href="#sub-vuex-support" class="link list__link">Vuex support</a></li>
-    <li class="list__element"><a href="#sub-action-dispatcher" class="link list__link">Action dispatcher</a></li>
-    <li class="list__element"><a href="#sub-custom-configuration" class="link list__link">Custom configuration</a></li>
-    <li class="list__element"><a href="#sub-programmatic-control" class="link list__link">Programmatic control</a></li>
+    <li class="list__element"><a href="#sub-option-groups" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-option-groups' }">Option groups</a></li>
+    <li class="list__element"><a href="#sub-vuex-support" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-vuex-support' }">Vuex support</a></li>
+    <li class="list__element"><a href="#sub-action-dispatcher" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-action-dispatcher' }">Action dispatcher</a></li>
+    <li class="list__element"><a href="#sub-custom-configuration" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-custom-configuration' }">Custom configuration</a></li>
+    <li class="list__element"><a href="#sub-programmatic-control" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-programmatic-control' }">Programmatic control</a></li>
     <li class="list__heading">API</li>
-    <li class="list__element"><a href="#sub-props" class="link list__link">Props</a></li>
-    <li class="list__element"><a href="#sub-events" class="link list__link">Events</a></li>
-    <li class="list__element"><a href="#sub-slots" class="link list__link">Slots</a></li>
+    <li class="list__element"><a href="#sub-props" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-props' }">Props</a></li>
+    <li class="list__element"><a href="#sub-events" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-events' }">Events</a></li>
+    <li class="list__element"><a href="#sub-slots" class="link list__link" :class="{ 'list__link--active': currentPosition === 'sub-slots' }">Slots</a></li>
     <li class="list__heading">Our other libraries</li>
     <li class="list__element"><a target="_BLANK" href="https://vuelidate.netlify.com"
                                  class="link list__link">Vuelidate<img
@@ -52,7 +54,8 @@ export default {
     return {
       isNavSticky: false,
       versions: ['v1.x', 'v2.0', 'v3.0'],
-      version: 'v3.0'
+      version: 'v3.0',
+      currentPosition: ''
     }
   },
   methods: {
