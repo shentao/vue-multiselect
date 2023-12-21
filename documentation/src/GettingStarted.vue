51d73e39db593e83b778552ff2169909284e09cf
Matt Elen <matt@suade.org>
2023-12-21 16:35:28 +0000

    docs(example): Add Prerequisites & Migration Guide

--- documentation/src/GettingStarted.vue
+++ documentation/src/GettingStarted.vue
@@ -6,6 +6,12 @@
         <hr class="typo__hr"/>
         <div class="grid__row">
           <div class="grid__column">
+            <h2 class="typo__h2">Prerequisites</h2>
+            <p>The multiselect from version 3.x requires Vue 3.0. If you need a version of the multiselect to support an
+              older version of Vue, check out 2.x versions</p>
+          </div>
+
+          <div class="grid__column">
             <h2 class="typo__h2">Installation</h2>
             <h3 class="typo__h3">via npm</h3>
             <pre>
@@ -54,6 +60,22 @@
 app.component('vue-multiselect', window.VueMultiselect.default)</code></pre>
           </div>
         </div>
+      </section>
+      <section id="sub-migration-guide" data-section="data-section">
+        <h1 class="typo__h1">Migration Guide</h1>
+        <hr class="typo__hr"/>
+
+        <p>If you are upgrading from vue-multiselect 2.x, there are only Vue 2 to Vue 3 Migration tasks to complete.
+          These include:</p>
+        <ul>
+          <li><code>value</code> prop is now <code>modelValue</code></li>
+          <li><code>@input</code> event is now <code>@update:modelValue</code></li>
+        </ul>
+        <p>If you are having issues with your migration, here are some tips and tricks to apply:</p>
+        <ul>
+          <li>Change props use kebab-case style</li>
+          <li>If you are using slots, check if your templates are setup correctly based of the examples below</li>
+        </ul>
       </section>
     </div>
   </div>
