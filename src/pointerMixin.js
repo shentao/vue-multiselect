176737e5aaf62efb0e779e17a9b4435983fcc764
Ignacio Durand <nachodurand@gmail.com>
2024-11-12 22:37:45 -0300

    Fix $isLabel issue
    When `this.filteredOptions[this.pointer]` is null, it's breaking on `pointerAdjust`

--- src/pointerMixin.js
+++ src/pointerMixin.js
@@ -129,6 +129,7 @@ export default {
       }
 
       if (this.filteredOptions.length > 0 &&
+        this.filteredOptions[this.pointer] &&  
         this.filteredOptions[this.pointer].$isLabel &&
         !this.groupSelect
       ) {
