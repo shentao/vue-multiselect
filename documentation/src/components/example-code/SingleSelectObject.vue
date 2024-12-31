ea226b978dab714ddd97272799e83c2c42248545
Matt Elen <matt@suade.org>
2024-12-31 15:29:31 +0000

    fix(#1776): Update docs with correct v-slot syntax

--- documentation/src/components/example-code/SingleSelectObject.vue
+++ documentation/src/components/example-code/SingleSelectObject.vue
@@ -2,7 +2,7 @@
   <div><label class="typo__label">Single select / dropdown</label>
     <multiselect v-model="value" deselect-label="Can't remove this value" track-by="name" label="name"
                  placeholder="Select one" :options="options" :searchable="false" :allow-empty="false">
-      <template slot="singleLabel" slot-scope="{ option }"><strong>{{ option.name }}</strong> is written in<strong>
+      <template v-slot:singleLabel="{ option }"><strong>{{ option.name }}</strong> is written in <strong>
         {{ option.language }}</strong></template>
     </multiselect>
     <pre class="language-json"><code>{{ value }}</code></pre>
