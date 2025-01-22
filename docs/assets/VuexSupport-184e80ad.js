59504352607525577f71556e85282f4486ef35b8
Matt Elen <matt@suade.org>
2025-01-22 16:37:43 +0000

    release(3-2): New build and doc files

---
+++ docs/assets/VuexSupport-184e80ad.js
@@ -0,0 +1,44 @@
+const e=`<template>
+<div><label class="typo__label">Vuex example.</label>
+    <multiselect id="vuex-support" placeholder="Pick action" :modelValue="value" :options="options" :searchable="false" @update:modelValue="updateValueAction" aria-label="pick a action"></multiselect>
+</div>
+</template>
+
+<script>
+/* Example of store setup
+const store = createStore({
+  state () {
+    return {
+      value: 'Vuex',
+      options: ['Vuex', 'Vue', 'Vuelidate', 'Vue-Multiselect', 'Vue-Router']
+    }
+  },
+  mutations: {
+    updateValue (state, value) {
+      state.value = value
+    }
+  },
+  actions: {
+    updateValueAction ({ commit }, value) {
+      commit('updateValue', value)
+    }
+  }
+})
+ */
+
+import Multiselect from 'vue-multiselect'
+import {mapActions, mapState} from 'vuex'
+
+export default {
+  components: {
+    Multiselect
+  },
+  computed: {
+    ...mapState(['value', 'options'])
+  },
+  methods: {
+    ...mapActions(['updateValueAction'])
+  }
+}
+<\/script>
+`;export{e as default};
